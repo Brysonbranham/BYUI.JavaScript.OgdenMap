63f0ba47c90b1bab640da1d6bba610f77f927769
Brysonbranham <104376155+bbranham4162@users.noreply.github.com>
2024-10-03 14:43:16 -0600

    Added Filter button and Readme

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# BYUI.JavaScript.OgdenMap
-Ogden Bus Map 
+## Overview
+
+**Rexburg Places of Interest**:
+
+**GIS Map Displaying some of my favorite local locations!**:
+
+**Map that runs and filters through 20 of my favorite places in the Rexburg and surrounding area.**:
+
+## Instructions for Build and Use
+
+Steps to build and/or run the software:
+
+1. Download and run VS Code
+2. Clone git repository at .....
+3. Download extension  Live Server by Ritwick Dey this will be used to host your map.
+4. Hold ctrl & shift + Press P. Then click the top option, live server.
+5. The server will automatically run in your local browser displaying your map!
+
+Instructions for using the software:
+
+1. Once the Map is loaded. Click and drag to move. 
+2. Click on orange location markers to display information
+3. Press the drop down box and select whichever category you want to filter to then select the filter button
+4. Huzzah! Only those locations are now being displayed! 
+
+## Development Environment 
+
+To recreate the development environment, you need the following software and/or libraries with the specified versions:
+
+* Visual Studio Code version 1.93
+* Live Server extension by Ritwick Dey
+* .net Extension v2.1.7
+
+## Useful Websites to Learn More
+
+I found these websites useful in developing this software:
+
+* [Esri Developer](https://developers.arcgis.com/javascript/latest/styling/)
+* [ArcGisOnline] (https://www.arcgis.com/index.html)
+* [ChatGPT] (https://chatgpt.com/)
+
+## Future Work
+
+The following items I plan to fix, improve, and/or add to this project in the future:
+
+* [ ] Add an image of each location that pops-up when the location is clicked on. 
+* [ ] Add ability for other people to add reviews for each place.
+* [ ] Add a button where you can add your own marker to create new points on the map.
--- RexburgPOI.js
+++ RexburgPOI.js
@@ -32,9 +32,10 @@ require(["esri/Map", "esri/views/MapView", "esri/Graphic", "esri/geometry/Point"
         width: 2
       }
     });
-  
-    
 
+   
+  
+  
 
   // Create a PopupTemplate for the POI
         var popupTemplate = new PopupTemplate({
@@ -57,26 +58,26 @@ require(["esri/Map", "esri/views/MapView", "esri/Graphic", "esri/geometry/Point"
         graphicsLayer.add(pointGraphic);
       }
 var POI = [
-  { longitude: -111.78303723114027, latitude: 43.82030010519513, name: "Planetarium" , description: "With 3 to four unique showings each month this is a must see date for any galaxy fans out there!"},
-  { longitude: -111.77589747564505, latitude: 44.15562113747064, name: "Civil Defense Caves" , description: "Home to some serious crevaces and even deadlier stalagmites and stalactites. It's quite the expedition for whichever adventurer!" },
-  { longitude: -111.79136030450309, latitude: 43.82300441847539, name: "Porter Park", description: "A peaceful park that attracts avid runners and Spikeball players alike!" },
-  { longitude: -111.77830142939811, latitude:  44.00740520424963, name: "Sand Dunes", description: "Want to take a girl star gazing? Or are you looking to go sledding? If so have I got the place for you! Perfect for both and for Summer bonfires!" },
-  { longitude: -111.79278194988042, latitude:  43.81850261290867, name: "Taco Truck", description: "An OG Taco truck. Want some delicious Mexican tacos, a taste of your old mission? Then this is the place for you" },
-  { longitude: -111.78148716217464, latitude:  43.81562316209788, name: "Ricks Gardens", description: "Peaceful place especially in the Spring and Summer. Feel free to come here and enjoy the sweet peace that envelopes one around God's nature." },
-  { longitude: -111.77469103792788, latitude:  43.85039325260046, name: "King's Roundup", description: "Great for beginners especially during their first 30 minutes of beginner classes! Open on Wednesdays and Fridays 8-12pm. " },
-  { longitude: -111.7804890756667, latitude:  43.836083764591024, name: "Fat Cats", description: "The place for all avid and not so avid minigolfers! Can be a little pricy but the fun is well worth it!" },
-  { longitude: -111.78529827751557, latitude: 43.83227539186468, name: "Rexburg Rapids", description: "Water slides and mini riptides! What more to say? " },
-  { longitude: -111.78279867239625, latitude:  43.818403221985456, name: " Rexburg RollerBlading", description: "Open Saturday and Monday 6:30pm-10:00pm. Come on down if you wanna twist your ankle! Or just wanna have some light fun!" },
-  { longitude: -111.78440434683236, latitude:  43.81865325463382, name: "VolleyBall", description: "Come on down almost any day of the week, besides Sunday. To enjoy some good ole fashioned Volleyball!" },
-  { longitude: -111.81027027333195 , latitude: 43.83087792746074, name: "The Rock Gym", description: "Rock climbing can be daunting especially for beginners. But I guarantee you wont forget it! A little on the pricy end but it makes for a memorable date!" },
-  { longitude: -111.77858592643412, latitude:  43.81167269238662, name: "Rexburg Temple", description: "A beautiful small temple. Home to one of the most peaceful gardens I've ever had the chance of relaxing at. One of my favorite spots to read." },
-  { longitude: -112.04117557568924, latitude:  43.50015413694857,  name: "Idaho Falls Temple", description: "Overlooking a beatiful River. Walks around this temple are not to be underestimated!" },
-  { longitude: -111.81714073334564, latitude:  43.718492775829596, name: "Haunted Maze", description: "Scary and exhilirating! A must for all who are easily scared!" },
-  { longitude: -111.67736108357137, latitude:  43.892774599445445, name: "Haunted Mill", description: "A scary Mill and even scarier barn! Claustrophobes beware!"},
-  { longitude: -110.57475840089901, latitude: 44.64059972197151, name: "YellowStone ", description: "Beautiful park, if you go you must see ole Faithful." },
-  { longitude: -111.9890501676114 , latitude: 43.78655324868936,  name: "R Mountain ", description: "A fun, light hike for beginners and experts alike!" },
-  { longitude: -111.78798371799655, latitude: 43.822160082829164, name: "Kiwi loco", description: "The premier first date spot! With good qualit froyo as a bonus" },
-  { longitude: -112.34425247571001, latitude:  43.19066050378673, name: "Oh Key! Escape Games ", description: "One of the most enjoyable dating spots in the area! Be careful though as this is where friendships go to die." }
+  { longitude: -111.78303723114027, latitude: 43.82030010519513, name: "Planetarium" , description: "With 3 to four unique showings each month this is a must see date for any galaxy fans out there!", Category: "Fun"},
+  { longitude: -111.77589747564505, latitude: 44.15562113747064, name: "Civil Defense Caves" , description: "Home to some serious crevaces and even deadlier stalagmites and stalactites. It's quite the expedition for whichever adventurer!", Category: "hike" },
+  { longitude: -111.79136030450309, latitude: 43.82300441847539, name: "Porter Park", description: "A peaceful park that attracts avid runners and Spikeball players alike!", Category: "Walk" },
+  { longitude: -111.77830142939811, latitude:  44.00740520424963, name: "Sand Dunes", description: "Want to take a girl star gazing? Or are you looking to go sledding? If so have I got the place for you! Perfect for both and for Summer bonfires!", Category: "Fun" },
+  { longitude: -111.79278194988042, latitude:  43.81850261290867, name: "Taco Truck", description: "An OG Taco truck. Want some delicious Mexican tacos, a taste of your old mission? Then this is the place for you", Category: "Food" },
+  { longitude: -111.78148716217464, latitude:  43.81562316209788, name: "Ricks Gardens", description: "Peaceful place especially in the Spring and Summer. Feel free to come here and enjoy the sweet peace that envelopes one around God's nature.", Category: "Walk" },
+  { longitude: -111.77469103792788, latitude:  43.85039325260046, name: "King's Roundup", description: "Great for beginners especially during their first 30 minutes of beginner classes! Open on Wednesdays and Fridays 8-12pm.", Category: "Fun" },
+  { longitude: -111.7804890756667, latitude:  43.836083764591024, name: "Fat Cats", description: "The place for all avid and not so avid minigolfers! Can be a little pricy but the fun is well worth it!", Category: "Fun" },
+  { longitude: -111.78529827751557, latitude: 43.83227539186468, name: "Rexburg Rapids", description: "Water slides and mini riptides! What more to say?", Category: "Fun" },
+  { longitude: -111.78279867239625, latitude:  43.818403221985456, name: " Rexburg RollerBlading", description: "Open Saturday and Monday 6:30pm-10:00pm. Come on down if you wanna twist your ankle! Or just wanna have some light fun!", Category: "Sports" },
+  { longitude: -111.78440434683236, latitude:  43.81865325463382, name: "VolleyBall", description: "Come on down almost any day of the week, besides Sunday. To enjoy some good ole fashioned Volleyball!", Category: "Sports" },
+  { longitude: -111.81027027333195 , latitude: 43.83087792746074, name: "The Rock Gym", description: "Rock climbing can be daunting especially for beginners. But I guarantee you wont forget it! A little on the pricy end but it makes for a memorable date!", Category: "Sports" },
+  { longitude: -111.77858592643412, latitude:  43.81167269238662, name: "Rexburg Temple", description: "A beautiful small temple. Home to one of the most peaceful gardens I've ever had the chance of relaxing at. One of my favorite spots to read.", Category: "Walk" },
+  { longitude: -112.04117557568924, latitude:  43.50015413694857,  name: "Idaho Falls Temple", description: "Overlooking a beatiful River. Walks around this temple are not to be underestimated!", Category: "Walk" },
+  { longitude: -111.81714073334564, latitude:  43.718492775829596, name: "Haunted Maze", description: "Scary and exhilirating! A must for all who are easily scared!", Category: "Fun" },
+  { longitude: -111.67736108357137, latitude:  43.892774599445445, name: "Haunted Mill", description: "A scary Mill and even scarier barn! Claustrophobes beware!", Category: "Fun"},
+  { longitude: -110.57475840089901, latitude: 44.64059972197151, name: "YellowStone ", description: "Beautiful park, if you go you must see ole Faithful.", Category: "Hike" },
+  { longitude: -111.9890501676114 , latitude: 43.78655324868936,  name: "R Mountain ", description: "A fun, light hike for beginners and experts alike!", Category: "Hike" },
+  { longitude: -111.78798371799655, latitude: 43.822160082829164, name: "Kiwi loco", description: "The premier first date spot! With good qualit froyo as a bonus", Category: "Food" },
+  { longitude: -112.34425247571001, latitude:  43.19066050378673, name: "Oh Key! Escape Games ", description: "One of the most enjoyable dating spots in the area! Be careful though as this is where friendships go to die.", Category: "Fun" }
   // Add more stops from your GTFS data
 ];
 
@@ -85,7 +86,30 @@ POI.forEach(function(place) {
 });
 
 
+
+
+document.getElementById('filter-button').addEventListener('click', function() {
+  const selectedCategory = document.getElementById('category-filter').value;
+  applyFilter(selectedCategory);
 });
+
+function applyFilter(category) {
+  // Clear existing POIs from the map
+// Clear existing POIs from the map
+graphicsLayer.removeAll(); // Clear existing graphics in the graphics layer
+
+// Filter POIs based on selected category
+const filteredPOIs = POI.filter(poi => {
+  return category === "all" || poi.Category.toLowerCase() === category.toLowerCase();
+});
+
+filteredPOIs.forEach(poi => {
+  addPOI(poi.longitude, poi.latitude, poi.name, poi.description);
+});
+}
+
+});
+
 
 
 
